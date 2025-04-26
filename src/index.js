742108f53cbca411e8783176375c4b4388c342a2
Devya <Devya@gmail.com>
2025-04-26 15:32:59 +0000

    image change

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(express.static('src'));
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
